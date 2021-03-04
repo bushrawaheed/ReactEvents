d727c34ca7dc63775ffc6ffdc46aca7465151d1f
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-03-04 10:11:18 +0100

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 export default function Forecast() {
-  function alertForecast() {
+  function alertForecast(event) {
+    event.preventDefault();
     alert("Forecast feature is not ready yet");
   }
   return (
